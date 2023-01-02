6fe12a73c58fe9abda44780e01655a425e9bc97b
nathanielfostanperry <natanielsevilla@gmail.com>
2023-01-01 19:19:53 -0600

    motion

--- src/components/ScrollArea.jsx
+++ src/components/ScrollArea.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
-import { motion, useScroll } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 const ScrollArea = () => {
-  const { scrollYProgress } = useScroll
+  const { scrollY } = useScroll()
+  const scrollContent = useTransform(
+    scrollY,
+    [0, 100],
+    [0, -1000],
+  )
+
+
 
   return (
     <div className=" bg-gray-200 relative">
-      <motion.div className="h-20 fixed top-0  bg-red-800"
-      style={{ scaleX: scrollYProgress }}
+      <motion.div className="h-[500px] w-40  from-red-800 to-indigo-500 bg-gradient-to-b"
+      style={{ translateY: scrollContent }}
+      >yo</motion.div>
+      <motion.div className="h-[500px] w-40  from-red-800 to-indigo-500 bg-gradient-to-b"
+      style={{ translateY: scrollContent }}
       >yo</motion.div>
       <div className="h-screen py-48"></div>
       <div className="h-screen py-48"></div>
